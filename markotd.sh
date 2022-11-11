8d58304554c236bf9eac180238ea0480fc12b20b
Stef Korporaal <stefkorporaal@gmail.com>
2022-11-11 14:17:11 +0100

    Add pacman update checker

--- markotd.sh
+++ markotd.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+PROGRAM="markotd"
+PARTS_FOLDER="markotd.d"
+NOTIFY_AFTER_DAYS=3
+
 # Common functions
 
 # Read and print lines from stdin.
@@ -32,13 +36,48 @@ get_dir() {
     return 1
 }
 
-PROGRAM="markotd"
-PARTS_FOLDER="markotd.d"
+# $1: date parseable by `date`
+seconds_since() {
+    date1="$(date +%s)"
+    date2=$(date -d  "$1" +%s)
+
+    echo "$((date1 - date2))"
+}
+
+# $1: date parseable by `date`
+hours_since() {
+    seconds="$(seconds_since "$1")"
+    hours=$((seconds / 60 / 60))
+
+    echo "$hours"
+}
+
+# $1: date parseable by `date`
+pretty_print_time_since_date() {
+    seconds_remaining=$(seconds_since "$1")
+
+    days=$(( seconds_remaining / 60 / 60 / 24 ))
+    seconds_remaining=$(( seconds_remaining % (60 * 60 * 24) ))
+    hours=$(( seconds_remaining / 60 / 60 ))
+    seconds_remaining=$(( seconds_remaining % (60 * 60) ))
+    minutes=$(( seconds_remaining / 60 ))
+    seconds=$(( seconds_remaining % (60) ))
+
+    date_string="";
+    [ "$days" -gt 0 ] && date_string="$date_string, $days days"
+    [ "$hours" -gt 0 ] && date_string="$date_string, $hours hours"
+    [ "$minutes" -gt 0 ] && date_string="$date_string, $minutes minutes"
+    [ "$seconds" -gt 0 ] && date_string="$date_string, $seconds seconds"
+    date_string=$(echo "$date_string" | sed 's/^, //' | rev | sed 's/ ,/ dna /' | rev)
+
+    echo "$date_string"
+}
 
 DIRS="$HOME/.$PARTS_FOLDER"
 
 EMPH=$(tput setaf 6)
 RESET=$(tput sgr0)
+NOTIFY_AFTER_HOURS="$(( NOTIFY_AFTER_DAYS * 24 ))"
 
 OUTPUT=$(
     if [ -n "$DEBUG" ]; then
